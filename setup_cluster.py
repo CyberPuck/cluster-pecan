7219f2c92a25a118417cf860739025ede2d16de2
Kyle Travers <traverskyle9@gmail.com>
2023-09-04 16:47:07 -0500

    feature/init-buildout: Network working.
    - IP addresses are assigned (DHCP)
    - Devices on network can communicate
    - Devices can get out of the network to Internet
    - VPN not working, seems to be messing with local network?
      misconfigured?

--- setup_cluster.py
+++ setup_cluster.py
@@ -1,5 +1,6 @@
 # TODO: Some roles need to be tagged?
 #   - main-node needs to be setup first with networking
+#   - This is being handled with the `setup_main_node.yml` file
 # TODO: Setup NAT (including IPTABLES)
 #   - This is a main-node task
 # TODO: Setup pivpn for remote cluster access
@@ -13,7 +14,10 @@
 # TODO: Setup k8s cluster
 #   - All nodes need:
 #   1. k8s repo accepted
+#       - Note: current k8s repo have moved from what is documented
 #   2. kubectl
 #   3. kubeadm
 #   4. kubelet
 #   5. kubernetes-cni
+
+# Break: Current setup is connecting to Internet from main-node, need to make sure dnsmasq setup is working correctly to forward to gateway
